76caa9c3b774c10f4c2f4fa0622154137c439dc3
Rinki4738 <rinkijha3315@gmail.com>
2025-08-01 23:25:05 +0530

    updated

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const path=require('path');
 const session=require('express-session');
 const flash=require('connect-flash');
 
-const seedDB=require('../seed');
-const productrouter=require('../routes/product');
+const seedDB=require('./seed');
+const productrouter=require('./routes/product');
 
 
-const cartrouter=require('../routes/cart');
-const paymentrouter = require('../routes/payment');
+const cartrouter=require('./routes/cart');
+const paymentrouter = require('./routes/payment');
 const ejsMate=require('ejs-mate');
 const methodOverride=require('method-override');
-const reviewrouter=require('../routes/review');
-const authrouter=require('../routes/auth');
+const reviewrouter=require('./routes/review');
+const authrouter=require('./routes/auth');
 const passport=require('passport');
 const LocalStrategy=require('passport-local')
-const User=require('../models/User')
+const User=require('./models/User')
 
 
 
